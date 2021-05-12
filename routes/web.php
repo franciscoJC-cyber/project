f712eb580740933df0b3fa392598ee6c5b19360c
Francisco <francisco.perez@jc-innovation.com>
2021-05-12 18:26:20 -0500

    cambios

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+});
+
+
